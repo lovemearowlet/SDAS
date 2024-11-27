ae280a40a7854205082f5b02fc2c30a603ff374e
SP649 <158019744+lovemearowlet@users.noreply.github.com>
2024-11-27 16:08:56 -0500

    Add files via upload

---
+++ YAGI src/config.c
@@ -0,0 +1,20 @@
+/*
+
+config syntax (its just C syntax);
+
+(type) (name and other stuff) = (value);
+
+this is for forkers and modders to add to. default has 2 options.
+
+
+*/
+
+//using ints cuz they are easier imo
+
+//option for doas or sudo: 0 is sudo 1 is doas
+//default is 0
+int sod = 0;
+
+//option for distro: 0 is gentoo 1 is arch
+//default is 0
+int dis = 0;
---
+++ YAGI src/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.c"
+
+int update(char **pkg) {//update
+	if (dis == 0) {//gentoo
+		if (sod == 0) {
+			//gentoo with sudo
+		}
+		if (sod == 1) {
+			//gentoo with doas
+		}
+	}
+	if (dis == 1) {//arch
+		if (sod == 0) {
+			//arch with sudo
+		}
+		if (sod == 1) {
+			//arch with doas
+		}
+	}
+}
+
+int install(char **pkg) {//install
+	if (dis == 0) {//gentoo
+		if (sod == 0) {
+			//gentoo with sudo
+		}
+		if (sod == 1) {
+			//gentoo with doas
+		}
+	}
+	if (dis == 1) {//arch
+		if (sod == 0) {
+			//arch with sudo
+		}
+		if (sod == 1) {
+			//arch with doas
+		}
+	}
+}
+
+int uninstall(char **pkg) {//uninstall
+	if (dis == 0) {//gentoo
+		if (sod == 0) {
+			//gentoo with sudo
+		}
+		if (sod == 1) {
+			//gentoo with doas
+		}
+	}
+	if (dis == 1) {//arch
+		if (sod == 0) {
+			//arch with sudo
+		}
+		if (sod == 1) {
+			//arch with doas
+		}
+	}
+}
